ede0ac2eaad39ae6bf5ae444545d7294e9758f86
Your Name <na.phuongdx@gmail.com>
2024-08-18 00:03:28 +0700

    update

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -3,22 +3,16 @@ function CategoryItem() {
   return (
     <div className='grid grid-cols-3 gap-4'>
       <div>
-        <h1 className="font-black pb-[30px] text-2xl">Shop</h1>
-        <p className="pb-[30px] text-[#555555]">Kiet ngu</p>
         <div>
           <img src="https://htmldemo.net/pullman/pullman/assets/img/category/group-1.jpg" className="w-full" />
         </div>
       </div>
       <div>
-        <h1 className="font-black pb-[30px] text-2xl">Shop</h1>
-        <p className="pb-[30px] text-[#555555]">Kiet ngu</p>
         <div>
           <img src="https://htmldemo.net/pullman/pullman/assets/img/category/group-1.jpg" className="w-full" />
         </div>
       </div>
       <div>
-        <h1 className="font-black pb-[30px] text-2xl">Shop</h1>
-        <p className="pb-[30px] text-[#555555]">Kiet ngu</p>
         <div>
           <img src="https://htmldemo.net/pullman/pullman/assets/img/category/group-1.jpg" className="w-full" />
         </div>
