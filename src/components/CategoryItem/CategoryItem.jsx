5262c4fd607265f030f97b2f3658b15a526cdf4f
Your Name <na.phuongdx@gmail.com>
2024-08-18 23:24:59 +0700

    update

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -5,16 +5,6 @@ import { FaCertificate } from "react-icons/fa";
 import { FaProductHunt } from "react-icons/fa";
 function CategoryItem() {
   return (
-    // <div className='grid grid-cols-3 gap-4'>
-    //   <div>
-    //     <h1 className="font-black pb-[30px] text-2xl"></h1>
-    //     <p className="pb-[30px] text-[#555555]"></p>
-    //     <div>
-    //       <img src="https://htmldemo.net/pullman/pullman/assets/img/category/group-1.jpg" className="w-full" />
-    //     </div>
-    //   </div>
-    //   <div>
-  
     <div className="grid grid-cols-4 gap-4 my-[150px] z-20 relative">
       <div className="w-full py-[75px] px-[15px] rounded-xl  hover:shadow-2xl bg-[#f4f6f8]">
         <div className="w-20 h-20 bg-[#FFB524] rounded-full flex justify-center items-center mx-auto">
